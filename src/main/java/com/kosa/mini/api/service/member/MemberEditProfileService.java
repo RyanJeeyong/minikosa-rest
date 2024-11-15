ed821adffda55e4dbfa15a75dcfcb95675cb4ea2
ksks1234 <138750403+ksks1234@users.noreply.github.com>
2024-11-15 09:47:57 +0900

    Revert "fix: 포스트맨에서 put 요청 후 내부서버 오류 발생"

--- src/main/java/com/kosa/mini/api/service/member/MemberEditProfileService.java
+++
@@ -1,8 +0,0 @@
-//package com.kosa.mini.api.service.member;
-//
-//import com.kosa.mini.mvc.domain.member.EditProfileDTO;
-//import com.kosa.mini.mvc.domain.member.Member;
-//
-//public interface MemberEditProfileService {
-//    }
-//
